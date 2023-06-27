4d48f28e07dd0eb65577da0e465be3528e6a5994
Mavludin <imevlud@gmail.com>
2023-06-27 13:10:56 +0300

    arrow functions

---
+++ 10. Arrow functions/script.js
@@ -0,0 +1,79 @@
+// Function expression
+
+// const func = function() {
+//     // Код
+// }
+
+// Стрелочные функции
+
+// const func = () => {
+
+// }
+
+// const func2 = (a) => {
+    
+// }
+
+// const func3 = a => {
+    
+// }
+
+// const func4 = (a, b, c) => {
+    
+// }
+
+// const func5 = () => {
+//     console.log('Строка кода - 1');
+//     console.log('Строка кода - 2');
+//     console.log('Строка кода - 3');
+// }
+
+// const func6 = () => console.log('Строка кода - 1');
+// const getSum = (a, b) => {
+//     return a + b
+// }
+
+// const getSum = (a, b) => a + b
+
+// ES6 (EcmaScript 2015)
+
+// const age = prompt("Сколько Вам лет?", 18);
+
+// const welcome = (age < 18) ?
+//   () => alert('Привет!') :
+//   () => alert("Здравствуйте!");
+
+// welcome();
+
+// let welcome;
+
+// if (age < 18) {
+//     welcome = () => alert('Привет!');
+// } else {
+//     welcome = () => alert("Здравствуйте!");
+// }
+
+// welcome()
+
+// if (age < 18) {
+//     alert('Привет!');
+// } else {
+//     alert("Здравствуйте!");
+// }
+
+// const age = prompt("Сколько Вам лет?", 18);
+
+// const welcome = (message) => alert(message);
+
+// welcome(age < 18 ? '13' : '23');
+
+// function welcome(message) {
+//     alert(message)
+// }
+
+// const welcome = function(message) {
+//     alert(message)
+// }
+
+// welcome(age < 18 ? 'привет!' : 'здравствуйте!');
+
