8e92b93b509b54c781b81c19a764fa2b7ecaf8c2
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2024-02-13 05:40:35 +0000

    Module 2. Topic 14

---
+++ Module 2/14. Methods of primitives. Number. String/script.js
@@ -0,0 +1,104 @@
+// let str = "MOSCOW"
+
+// str.toUpperCase()
+
+// str String()
+// .toUpperCase()
+// String() X
+
+// console.log(str.toLowerCase())
+
+// const value = Number(0)
+
+// console.log(typeof Number("1994"))
+
+
+// Числа
+
+// let num = 9e3
+
+// console.log(num)
+
+// let num = 0.1 + 0.2
+// console.log(num === 0.3)
+
+// let num = 9000
+
+// console.log(typeof num.toString())
+
+// console.log(new Date)
+
+// 9000..toString()
+
+// console.log(Math.floor(3.5))
+
+// console.log(Math.ceil(3.5))
+
+// console.log(Math.round(3))
+
+// let num = 9.4124214
+
+// console.log(999_999_999_999_9999)
+
+// alert(999_999_999_999_9999)
+
+// console.log(isNaN(NaN))
+
+// console.log(parseInt('px100'))
+
+// if (parseInt('px100')) {
+//   console.log('Есть')
+// }
+
+// console.log(Math.random())
+// console.log(Math.max(1, 10))
+// console.log(Math.pow(2, 3))
+
+
+// Строки
+
+// let str1 = 'Первый'
+// let str2 = "Второй"
+// let str3 = `Третьи`
+
+// let num = 1994
+
+// let birthday = `Год рождения: ${num}`
+
+// console.log(birthday)
+
+// let name = "   Murad Arslangirav      "
+// name[4] = 'r'
+
+// console.log(name[4] = 'r')
+// console.log(name.length)
+
+// console.log(name)
+
+// console.log(name.toLowerCase())
+
+// console.log(name.toUpperCase())
+
+// console.log(name.indexOf('a'))
+// console.log(name.lastIndexOf('a'))
+
+// console.log(name.includes('Murad'))
+// console.log(name.startsWith('Murad'))
+// console.log(name.endsWith('v'))
+
+// console.log(name.slice(0, 255))
+// console.log(name.substr(0, 5))
+// console.log(name.substring(0, 5))
+
+// console.log('a' > 'Z')
+
+
+// let name1 = "A"
+// let name2 = "a"
+
+
+// console.log(name1.localeCompare(name2))
+
+// console.log(name.trim())
+
+
