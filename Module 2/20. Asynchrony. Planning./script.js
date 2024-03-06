b5b665edb620270ae15242379aaddd2789a61d4a
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2024-03-06 11:15:54 +0000

    Module 2. Topic 20

---
+++ Module 2/20. Asynchrony. Planning./script.js
@@ -0,0 +1,42 @@
+// Синтаксис setTimeout
+
+// setTimeout(function() {console.log('Первый вариант')}, 2000)
+// setTimeout(() => console.log('Второй вариант'), 2000)
+// let timer = setTimeout(function() {console.log('Третьи вариант')}, 2000)
+
+// function timer() {
+//   console.log("Четвертый вариант")
+// }
+
+// setTimeout(timer, 2000)
+
+// Очищение setTimeout
+
+// let timer = setTimeout(function() {console.log('Первый вариант')}, 2000)
+// clearTimeout(timer)
+
+
+// Синтаксис setInterval
+
+// setTimeout(function() {console.log('Первый вариант')}, 2000)
+// setTimeout(() => console.log('Второй вариант'), 2000)
+// let timer = setInterval(function() {console.log('Третьи вариант')}, 2000)
+
+// function timer() {
+//   console.log("Четвертый вариант")
+// }
+
+// setInterval(timer, 2000)
+
+// Очищение setInterval
+
+// let timer = setInterval(function() {console.log('Первый вариант')}, 2000)
+// clearTimeout(timer)
+
+
+// Далее обсудили работу event loop. 
+// Ссылки которые помогут вам закрепить эту тему. 
+// https://youtu.be/vIZs5tH-HGQ?si=C16RSfUaUDLxPAbi
+// https://youtu.be/8aGhZQkoFbQ?si=b7tLddv1HNeV02ak
+
+// Также в чате скину изображение с механизмами event loop с хэштегом #eventLoop
