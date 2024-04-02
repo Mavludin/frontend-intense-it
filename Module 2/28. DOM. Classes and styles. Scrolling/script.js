271461db0ccd88f4cbf0e37ca2907c21b09bc78d
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2024-04-02 13:49:00 +0000

    Module 2. Topic 28

---
+++ Module 2/28. DOM. Classes and styles. Scrolling/script.js
@@ -0,0 +1,84 @@
+// Добавление стилей в атрибут style 
+
+// const box = document.querySelector('.box')
+
+// box.style.height = "100px"
+// box.style.width = "100px"
+// box.style.backgroundColor = "red"
+// box.style.MozAnimation = "alternative"
+
+// box.style.cssText = `
+//   height: 100px;
+//   width: 100px;
+//   background-color: red
+// `
+
+// console.log(box.classList)
+
+// add 
+// box.classList.add("newName")
+
+// remove
+// box.classList.remove("box")
+
+// toggle
+// box.classList.toggle("box")
+
+// contains
+// console.log(box.classList.contains("box"))
+
+// Получение стилей
+
+// console.log(box.style.height)
+
+// const boxStyle = getComputedStyle(box)
+
+// console.log(boxStyle.height)
+// console.log(boxStyle.backgroundColor)
+
+
+// Размеры и прокрутка элементов
+
+// const box = document.querySelector('.box')
+
+// Внешние метрики
+
+// offsetParent
+// offsetLeft/Top
+
+// console.log(box.offsetParent)
+// console.log(box.offsetLeft)
+// console.log(box.offsetTop)
+
+// Внутренние метрики
+
+// offsetWidth/Height
+
+// console.log(box.offsetWidth)
+// console.log(box.offsetHeight)
+
+// clientTop/Left
+// console.log(box.clientTop)
+// console.log(box.clientLeft)
+
+// clientWidth/Height
+
+// console.log(box.clientWidth)
+// console.log(box.clientHeight)
+
+// scrollWidth/Height
+
+// console.log(box.scrollWidth)
+// console.log(box.scrollHeight)
+
+
+// scrollLeft/Top
+
+// console.log(box.scrollLeft)
+// console.log(box.scrollTop)
+
+// const btn = document.querySelector('.btn')
+
+// btn.addEventListener("click" , () => {
+//   box.scrollTop = 0
+// })
