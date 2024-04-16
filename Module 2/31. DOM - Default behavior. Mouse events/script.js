9216a5a1a726f845d7261782f63497a418ed645d
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2024-04-16 14:48:18 +0000

    Module 2. Topic 31

---
+++ Module 2/31. DOM - Default behavior. Mouse events/script.js
@@ -0,0 +1,64 @@
+// const link = document.querySelector('.link')
+
+// link.addEventListener('click', (e) => {
+//     e.preventDefault()
+
+//     console.log('не перешёл', e)
+// })
+
+// const form = document.querySelector('form')
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+// })
+
+
+// const box = document.querySelector('.box')
+
+// box.addEventListener('click', (e) => {
+//     e.preventDefault()
+
+//     if (e.target.tagName === "A") {
+//         e.target.getAttribute('href')
+//     }
+// })
+
+// link.addEventListener('click', (e) => {
+//     e.preventDefault()
+
+//     console.log('defaultPrevented', e.defaultPrevented)
+// })
+
+// Событие мыши
+
+// box.addEventListener("mousedown", (e) => {
+//     console.log("Кнопка мыши нажата")
+// })
+
+// box.addEventListener("mouseup", (e) => {
+//     console.log("Кнопка мыши отпущена")
+// })
+
+// box.addEventListener("mouseover", (e) => {
+//     console.log("Курсор мыши появляется над элементом")
+// })
+
+// box.addEventListener("mouseout", (e) => {
+//     console.log("Курсор мыши уходит с элемента")
+// })
+
+// box.addEventListener("mousemove", (e) => {
+//     console.log("Курсор мыши над элементом")
+// })
+
+// box.addEventListener("click", (e) => {
+//     console.log("Клик по элементу")
+// })
+
+// box.addEventListener("dblclick", (e) => {
+//     console.log("Вызывается двойным кликом на элементе")
+// })
+
+// box.addEventListener("contextmenu", (e) => {
+//     console.log("Нажатие правой кнопки мыши")
+// })
