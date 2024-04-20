d02d2171618ff4bcfabb7f541336f5991480d4ee
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2024-04-20 16:02:43 +0000

    Module 2. Topic 32

---
+++ Module 2/32. DOM - Mouse events. Key events./script.js
@@ -0,0 +1,74 @@
+const box1 = document.querySelector(".box1")
+const box2 = document.querySelector(".box2")
+
+let count = 0
+
+// box1.addEventListener("mouseover", (event) => {
+//     count += 1
+//     console.log(count)
+// })
+
+// box2.addEventListener("mouseout", (event) => {
+//     console.log(event)
+//     console.log(event.relatedTarget)
+
+//     if (event.relatedTarget === null) {
+//         return 
+//     } 
+// })
+
+// box1.addEventListener("mousemove", (event) => {
+//     count += 1
+//     console.log(count)
+// })
+
+// box1.addEventListener("mouseenter", (event) => {
+//     console.log(event)
+// })
+
+// box2.addEventListener("mouseleave", (event) => {
+//     console.log(event)
+// })
+
+/////////////
+// Клавиатурные события
+
+
+const input = document.querySelector("input")
+
+// input.addEventListener("input", (e) => {
+//     console.log(e)
+// })
+
+// input.addEventListener("keydown", (e) => {
+//     triggerWindow(e)
+// })
+
+// input.addEventListener("keyup", (e) => {
+//     console.log(e)
+// })
+
+// input.addEventListener("keypress", (e) => {
+//     // console.log(e)
+//     // triggerWindow(e.key)
+//     // triggerWindow(e.keyCode)
+//     triggerWindow(e)
+// })
+
+// function triggerWindow(event) {
+//     console.log(event)
+//     console.log(eventKey)
+//     if (eventKey === "A" || eventKey === "G") {
+//         console.log("Выводится модалка")
+//     }
+
+//     if (eventKey === 97 || eventKey === 103) {
+//         console.log("Выводится модалка")
+//     }
+
+//     if (event.altKey && event.keyCode === 65) {
+//         console.log("выполняется процедура")
+//     }
+// }
+
+
