af3652cb1004068f65a93875fe39e79e1e95bbdf
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2024-04-23 18:06:55 +0000

    Module 2. Topic 33

---
+++ Module 2/33. DOM - Introduction to forms/script.js
@@ -0,0 +1,49 @@
+// let btnSbt = document.querySelector('.btnSbt')
+
+// btnSbt.addEventListener("submit", (e) => {
+//     console.log(document.userInfo.user.value)
+// })  
+
+// Доступ к форме
+// console.log(document.userInfo)
+// Доступ к полю через форму
+// console.log(document.userInfo.user.value)
+
+// Получение данных radio
+// let form = document.forms
+// console.log(form[1].moscow.value)
+
+
+// Обратная ссылка: element.form
+// let nameForm = document.form.user
+// console.log(nameForm)
+// console.log(nameForm.form)
+
+// Получение полей формы
+// let nameForm = document.form.user
+// let descForm = document.form.description
+// let maleForm = document.form.male
+
+// Переопределяем value у инпутов
+// nameForm.value = "Mike"
+// descForm.value = "Описание"
+// Переопределяем value у инпута чекбокса
+// maleForm.checked = true
+
+// Select
+// let selectForm = document.form.select
+// selectForm.value = "second"
+// console.log(selectForm)
+
+// Решаем задачу из learnJavaScript
+// let genres = document.querySelector('#genres')
+// let selectedOption = genres.options[genres.selectedIndex]
+
+// let newOption = new Option("Классика", "classic")
+
+// console.log(selectedOption.value)
+// console.log(selectedOption.textContent)
+
+// genres.append(newOption)
+
+// newOption.selected = true
