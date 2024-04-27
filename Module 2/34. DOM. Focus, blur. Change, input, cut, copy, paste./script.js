53c93687624c636f6e812a0986c723cf95cc938a
webdevmurad <36207098+webdevmurad@users.noreply.github.com>
2024-04-27 10:45:05 +0000

    Module 2. Topic 34

---
+++ Module 2/34. DOM. Focus, blur. Change, input, cut, copy, paste./script.js
@@ -0,0 +1,61 @@
+// change, input, cut, copy, paste
+
+// let input = document.querySelector(".input")
+// let select = document.querySelector(".select")
+
+// input.addEventListener("change", () => {
+//     console.log("Change")
+// })
+
+// select.addEventListener("change", () => {
+//     console.log("Change")
+// })
+
+// input.addEventListener("input", (e) => {
+//     console.log(e.data)
+//     console.log(e.target.value)
+// })
+
+// input.addEventListener("cut", (e) => {
+//     e.preventDefault()
+//     console.log("Cut")
+
+//     // Вырезание значения из инпута
+// })
+
+// input.addEventListener("copy", (e) => {
+//     e.preventDefault()
+//     console.log("Copy")
+
+//     // Копирование значения из инпута
+// })
+
+// input.addEventListener("paste", (e) => {
+//     e.preventDefault()
+//     console.log("Paste")
+
+//     // Вставить значение в инпут
+// })
+// getName()
+
+// focus/blur
+
+
+// let input1 = document.querySelector(".input1")
+// let input2 = document.querySelector(".input2")
+
+
+// input1.addEventListener('blur', (e) => {
+//     if (!e.target.value.includes("@")) {
+//         input1.classList.add("error")
+//         console.log("Некорректное значение")
+//     }
+ 
+//     console.log("blur")
+// })
+
+// input1.addEventListener('focus', () => {
+//     input1.classList.remove("error")
+
+//     console.log("focus")
+// })
